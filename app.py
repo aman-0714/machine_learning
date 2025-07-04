0204338370fbe3ed78731ccf0d678cbff4057ded
aman-0714 <akpreet1404@gmail.com>
2025-07-04 17:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import xgboost
 from xgboost import XGBRegressor
 import pandas as pd
-pipe = pickle.load(open('pipe.pkl','rb'))
+pipe = pickle.load(open('pipe_01.pkl','rb'))
 
 teams = ['Australia',
  'India',
